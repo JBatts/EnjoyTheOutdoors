b59ff382372b6f22ac95f377bdbe2809056c6c50
Jalen Battle <jalenbattle78@gmail.com>
2024-11-01 16:00:16 -0400

    National Parks page is now functional

---
+++ scripts/main.js
@@ -0,0 +1,86 @@
+"use strict"
+
+document.addEventListener("DOMContentLoaded", ()=>{
+    const parkSelect1 = document.getElementById("parkSelect1")
+    const parkSelect2 = document.getElementById("parkSelect2")
+    const parkDetails = document.getElementById("parkDetails")
+    const parkBody = document.getElementById("parkBody")
+    const reset = document.getElementById("reset")
+    
+    locationsArray.forEach(state => {
+        const option = document.createElement("option")
+        option.value = state
+        option.innerHTML = state
+        parkSelect1.appendChild(option)
+    });
+
+    parkTypesArray.forEach(parkType => {
+        const option = document.createElement("option")
+        option.value = parkType
+        option.innerHTML = parkType
+        parkSelect2.appendChild(option)
+    });
+
+    function filterAndDisplayParks(){
+        const selectedLoc = parkSelect1.value
+        const selectedParkType = parkSelect2.value
+
+        parkBody.innerHTML = "";
+
+        const filteredParks = nationalParksArray.filter(park => {
+            const matchesLoc = selectedLoc === "Please Select Below" || park.State === selectedLoc
+            const matchesType = selectedParkType === "Please Select Below" || park.LocationName.includes(selectedParkType)
+            return matchesLoc && matchesType;
+        });
+
+        filteredParks.forEach(park => {
+            const row = document.createElement("tr")
+
+            const nameCell = document.createElement("td");
+            nameCell.innerHTML = park.LocationName;
+            row.appendChild(nameCell);
+
+            const cityCell = document.createElement("td");
+            cityCell.innerHTML = park.City;
+            row.appendChild(cityCell);
+
+            const addressCell = document.createElement("td");
+            addressCell.innerHTML = park.Address;
+            row.appendChild(addressCell);
+
+            const phoneCell = document.createElement("td");
+            if(park.Phone != 0){
+                phoneCell.innerHTML = park.Phone
+            } else {
+                phoneCell.innerHTML = "N/A"
+            };
+            row.appendChild(phoneCell);
+
+            const websiteCell = document.createElement("td");
+            if(park.Visit) {
+                const link = document.createElement("a")
+                link.href = park.Visit;
+                link.innerHTML = "Hyperlink"
+                websiteCell.appendChild(link);
+            } else {
+                websiteCell.innerHTML = "N/A"
+            }
+            row.appendChild(websiteCell)
+
+            // Adds it all inforamtion to the table
+            parkBody.appendChild(row)
+        });
+        parkDetails.style.display = filteredParks.length > 0 ? "block" : "none"
+    };
+
+    function resetTable(){
+        parkSelect1.value = "Please Select Below"
+        parkSelect2.value = "Please Select Below"
+        parkBody.innerHTML = ""
+        parkDetails.style.display = "none"
+    }
+
+    parkSelect1.addEventListener("change", filterAndDisplayParks)
+    parkSelect2.addEventListener("change", filterAndDisplayParks)
+    reset.addEventListener("click", resetTable)
+});
